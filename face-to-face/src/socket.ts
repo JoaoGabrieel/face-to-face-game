499e3c8d196fb09f49a72327ae852975e74d6e40
Joao Gabriel <jgrs2003@hotmail.com>
2026-08-20 21:45:31 -0300

    fix:...

--- face-to-face/src/socket.ts
+++ face-to-face/src/socket.ts
@@ -1,7 +1,5 @@
 import { io, Socket } from "socket.io-client";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL ?? "http://localhost:3001";
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
 
-export const socket: Socket = io(SERVER_URL, {
-  autoConnect: false,
-});
+export const socket = io(SERVER_URL, { autoConnect: false });
